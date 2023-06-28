6887d1e40f59cb39d9e7b3856485429ef774f1e0
devarsh shingala <shingaladevarsh1@gmail.com>
2023-06-29 01:10:58 +0530

    new updates done, accouting is hidden now

--- src/app/project-accounting/project-accounting.component.ts
+++ src/app/project-accounting/project-accounting.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-project-accounting',
   templateUrl: './project-accounting.component.html',
-  styleUrls: ['./project-accounting.component.css']
+  styleUrls: ['./project-accounting.component.css'],
 })
-export class ProjectAccountingComponent {
-
-}
+export class ProjectAccountingComponent {}
